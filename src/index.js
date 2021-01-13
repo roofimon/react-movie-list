b9c1fb5ddcf5da2670368a2e6016bbebd591d9e4
RooF <roofimon@gmail.com>
2021-01-13 14:47:34 +0700

    Add containers and components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './containers/List';
 
 const App = () => {
-    return <h1>Movie List</h1>;
+    //return <h1>Movie List</h1>;
+    return  <List />
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
