9bd20f500a43a251e84ee48e997edb2919bd7b4a
RooF <roofimon@gmail.com>
2021-01-13 15:41:24 +0700

    Fetch data

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import Card from '../components/Card';
 
 class List extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: [],
+            loading: true,
+        };
+    }
+
+    async componentDidMount() {
+        console.log("Did Mount");
+        const movies = await fetch('../../assets/data.json');
+        const moviesJSON = await movies.json();
+
+        if(moviesJSON) {
+            this.setState({
+                data: moviesJSON,
+                loading: false,
+            });
+        }
+    }
+
     render() {
-        return <Card />;
+        const { data, loading } = this.state;
+
+        if (loading) {
+            return <div>Loading ...</div>
+        }
+
+        return data.map(movie => <Card key={ movie.id } movie={ movie} />);
     }
 };
 
